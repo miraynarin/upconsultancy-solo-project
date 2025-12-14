3eceac74d4a287e824c62890c4898395ccd0ad1b
miraynarin <narinmiray@hotmail.com>
2025-12-14 23:12:13 +0300

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
-
+import './navbar.css'; 
 const Navbar = () => {
     const container = document.createElement("div")
     container.innerHTML = `
     <header> 
-    <div class="logo">
-    UpConsultancy
-    </div>
+    <div class="logo"> UpConslt </div>
+    <div class="logo1"> Services </div>
+    <div class="logo1"> Pricing </div>
+    <div class="logo1"> Blog </div>
+    <div class="logo1"> Login <div>
     </header> ` 
     return container
 }
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,3 +1,4 @@
+import "../../styles/style.css"; 
 import Navbar from "../../components/Navbar/Navbar"
 
 const HomePage = () => {
