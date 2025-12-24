3dd83d6ef55a896e30aceee836c038b759b7ba5f
miray <miray@users.noreply.github.com>
2025-12-24 11:57:07 +0300

    Navbar css and responsive updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,19 +2,33 @@ import './navbar.css';
 const Navbar = () => {
     const container = document.createElement("div")
     container.innerHTML = `
-    <header class="navbar"> 
-        <div class="logo">UpConslt</div>
+    <header class="header-container"> 
+    <div class="navbar">
+    <div class="navbar-left-area">
+        <a class="logo" href="/">UpConslt</a>
         <div class="navbar-group">
+         
             <div class="menu">
-                <div class="frame">Services</div>
-                <div class="frame">Pricing</div>
-                <div class="frame">Blog</div>
-                <div class="frame">Login</div>
+                <a class="frame" href="/services">Services</a>
+                <a class="frame" href="#">Pricing</a>
+                <a class="frame" href="#">Blog</a>
+                <a class="frame" href="#">Login</a>
             </div>
-            <div class="hamburger" id="hamburgerIcon"> ☰ </div>
+            
+        </div></div>
+          <div class="navbar-right-area">
+       <div class="hamburger" id="hamburgerIcon"> ☰ </div>
             <button class="btn-getstarted">Get Started</button>
         </div>
+        </div>
     </header> `
+   const hamburger = container.querySelector("#hamburgerIcon")
+const menu = container.querySelector(".menu")
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active")
+})
+
     return container
 }
 export default Navbar
