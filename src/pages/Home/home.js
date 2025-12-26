0a19506f6f6860e6ac7a1b7989fff5acfa72f3af
miray <miray@users.noreply.github.com>
2025-12-26 23:05:15 +0300

    number added

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,17 +1,19 @@
-import "../../styles/style.css"; 
-import Navbar from "../../components/Navbar/Navbar"
+import "../../styles/style.css";
+import Navbar from "../../components/Navbar/Navbar";
 import HeroSection from "../../section/HeroSection/HeroSection";
-import Services  from "../../section/Services/Services";
-
+import Services from "../../section/Services/Services";
+import Number from "../../section/Number/Number";
 
 const HomePage = () => {
-    const container = document.createElement("div")
-   const navbar = Navbar ()
-   const heroSection = HeroSection()
-   const services = Services()
-   container.appendChild(navbar)
-   container.appendChild(heroSection)
-   container.appendChild(services)
-    return container
-}
-export default HomePage
+  const container = document.createElement("div");
+  const navbar = Navbar();
+  const heroSection = HeroSection();
+  const services = Services();
+  const number = Number();
+  container.appendChild(navbar);
+  container.appendChild(heroSection);
+  container.appendChild(services);
+  container.appendChild(number);
+  return container;
+};
+export default HomePage;
---
+++ src/section/Number/Number.js
@@ -0,0 +1,35 @@
+import "./Number.css";
+const Number = () => {
+  const container = document.createElement("div");
+  container.innerHTML = `
+    <section class="number-section">
+    <div class="number-content">
+    <h3 class="number-first-title">Why Us?</h3>
+    <h2 class="number-title">We Have a Lot of Experience in Work</h2>
+    <p class="number-text">UpConslt as a leading business consulting place, we have helped many small to<br>medium and large companies to help their business</p>
+    
+    <div class="number-value-part">
+    <div class="number-value">
+    <h2>1200</h2>
+    <h3>Projects Completed</h3>
+    </div>
+    <div class="number-value">
+    <h2>20</h2>
+    <h3>Year of Experience</h3>
+    </div>
+    <div class="number-value">
+    <h2>55</h2>
+    <h3>New Brand</h3>
+    </div>
+    <div class="number-value">
+    <h2>14</h2>
+    <h3>Awwards Won</h3>
+    </div>
+    </div>
+    </div>
+    </section>
+    
+    `;
+  return container;
+};
+export default Number;
