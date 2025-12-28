9f86d1f1b849c25853c251d4e206ca833e419cd1
miray <miray@users.noreply.github.com>
2025-12-28 18:33:15 +0300

    more feature added

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -4,6 +4,7 @@ import HeroSection from "../../section/HeroSection/HeroSection";
 import Services from "../../section/Services/Services";
 import Number from "../../section/Number/Number";
 import Features from "../../section/Features/Features";
+import MoreFeature from "../../section/MoreFeatures/MoreFeatures";
 
 const HomePage = () => {
   const container = document.createElement("div");
@@ -12,11 +13,13 @@ const HomePage = () => {
   const services = Services();
   const number = Number();
   const features = Features();
+  const moreFeature = MoreFeature();
   container.appendChild(navbar);
   container.appendChild(heroSection);
   container.appendChild(services);
   container.appendChild(number);
   container.appendChild(features);
+  container.appendChild(moreFeature);
   return container;
 };
 export default HomePage;
