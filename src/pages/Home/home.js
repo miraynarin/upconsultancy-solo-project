267feb95cc38ade44f5a74e95bf2aca4d9a5a93c
miray <miray@users.noreply.github.com>
2025-12-30 15:34:47 +0300

    pricing added

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,6 +5,7 @@ import Services from "../../section/Services/Services";
 import Number from "../../section/Number/Number";
 import Features from "../../section/Features/Features";
 import MoreFeature from "../../section/MoreFeatures/MoreFeatures";
+import Pricing from "../../section/Pricing/Pricing";
 
 const HomePage = () => {
   const container = document.createElement("div");
@@ -14,12 +15,14 @@ const HomePage = () => {
   const number = Number();
   const features = Features();
   const moreFeature = MoreFeature();
+  const pricing = Pricing();
   container.appendChild(navbar);
   container.appendChild(heroSection);
   container.appendChild(services);
   container.appendChild(number);
   container.appendChild(features);
   container.appendChild(moreFeature);
+  container.appendChild(pricing);
   return container;
 };
 export default HomePage;
