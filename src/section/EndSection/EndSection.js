e99f132e3e8a6e3dd2af2aacd17b2b9b724623f4
miray <miray@users.noreply.github.com>
2025-12-31 23:02:33 +0300

    FAQ section updated

--- src/section/EndSection/EndSection.js
+++ src/section/EndSection/EndSection.js
@@ -8,11 +8,16 @@ const EndSection = () => {
   <h3 class="faq-text">Do you have any question?</h3>
   <div class="faq-question">
   <div class="question">What is subscription pricing or a subscription-based pricing model?
-  <img src="./images/arrow_back_icon.png" class="back-icon"></div>
+  <button class="end-section-button">
+  <img src="./images/arrow_back_icon.png" class="back-icon"></button></div>
   <div class="question">Why adopt a subscription business model?
-  <img src="./images/arrow_back_icon.png" class="back-icon"></div>
+  <button class="end-section-button">
+  <img src="./images/arrow_back_icon.png" class="back-icon"></button></div>
+  <div class="question-explanation">One, it attracts more customers with its lower entry point (e.g. paying $15 monthly vs. $150 in advance). Two, it offers predictable revenue for businesses. Three, it allows businesses to build stronger, more valuable relationships with its customers.</div>
   <div class="question">How does a subscription management model work?
-  <img src="./images/arrow_back_icon.png" class="back-icon"></div>
+  <button class="end-section-button">
+  <img src="./images/arrow_back_icon.png" class="back-icon"></button></div>
+  <div class="question-explanation">One, it attracts more customers with its lower entry point (e.g. paying $15 monthly vs. $150 in advance). Two, it offers predictable revenue for businesses. Three, it allows businesses to build stronger, more valuable relationships with its customers.</div>
   </div>
    </section>
    
