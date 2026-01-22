5ddad23474d00868021b515a230e2533d5c271d4
miray <miraynarin@users.noreply.github.com>
2026-01-22 22:27:23 +0300

    sign-up page css updated

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import page from "page";
 import HomePage from "./pages/Home/home";
 import ServicePage from "./pages/Services/Services";
 import LoginPage from "./pages/Login/Login";
+import SignUpPage from "./pages/SignUp/SignUp";
+
 const app = document.querySelector("#app");
 page("/", async () => {
   app.replaceChildren(HomePage());
@@ -11,5 +13,8 @@ page("/services", async () => {
 });
 page("/login", async () => {
   app.replaceChildren(LoginPage());
+});
+page("/sign-up", async () => {
+  app.replaceChildren(SignUpPage());
 });
 page();
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,6 @@
-import "../../styles/style.css";
-import "../../pages/Login/login.css";
+import "./SignUp.css";
 
-const LoginPage = () => {
+const SignUpPage = () => {
   const container = document.createElement("div");
   container.innerHTML = `
      <section class="login-section">
@@ -10,10 +9,11 @@ const LoginPage = () => {
    
      <div class="right-area">
       <h3>UpConslt</h3>
-      <h1>Login</h1>
+      <h1>Sign up</h1>
      <form>
-     <input type="text" placeholder="Email or phone number" /> 
-    
+     <input type="text" placeholder="Name" /> 
+     <input type="number" placeholder="Phone Number" /> 
+     <input type="email" placeholder="Email" /> 
      <div class="password-wrapper">
      <input type="password" id="password" placeholder="Enter Password" />
      <span id="togglePassword" class="eye-icon">👁️</span>
@@ -25,11 +25,11 @@ const LoginPage = () => {
      <span class="remember-text">Remember me</span>
      <a href="#" class="forgot-password">Forgot password?</a>
      </form>
-     <button class="login-button">Login</button>
-     <button class="google-button">Or Login with Google</button>
+     <button class="login-button">Sign in</button>
+     <button class="google-button">Or Sing in with Google</button>
      <div class="login-footer">
-     <p class="account">Don't have an account?</p>
-     <a href="/sign-up" class="sign-up">Sign up now</p>
+     <p class="account">Do you have an account?</p>
+     <a href="/login" class="sign-up">Sign up now</p>
      </div>
    </div>
    </section>`;
@@ -44,4 +44,4 @@ const LoginPage = () => {
 
   return container;
 };
-export default LoginPage;
+export default SignUpPage;
