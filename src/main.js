6a9190e50df34608e4dbbc7f16cd6a80b9490764
miray <miraynarin@users.noreply.github.com>
2026-02-04 21:42:27 +0300

    BlogPage added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import page from "page";
 import HomePage from "./pages/Home/home";
 import ServicePage from "./pages/Services/Services";
 import PricingPage from "./pages/PricingPage/PricingPage";
+import BlogPage from "./pages/BlogPage/BlogPage";
 import LoginPage from "./pages/Login/Login";
 import SignUpPage from "./pages/SignUp/SignUp";
 
@@ -15,7 +16,9 @@ page("/services", async () => {
 page("/pricing", async () => {
   app.replaceChildren(PricingPage());
 });
-
+page("/blog", async () => {
+  app.replaceChildren(BlogPage());
+});
 page("/login", async () => {
   app.replaceChildren(LoginPage());
 });
