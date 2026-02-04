232c7fa9b099c02c60f351c3d4d10415f7d8c2c0
miray <miraynarin@users.noreply.github.com>
2026-02-04 12:04:20 +0300

    Added Navbar, Footer, NewsletterSection, EndSection, ContactSection, and ContactInfo to the Pricing page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import page from "page";
 import HomePage from "./pages/Home/home";
 import ServicePage from "./pages/Services/Services";
+import PricingPage from "./pages/PricingPage/PricingPage";
 import LoginPage from "./pages/Login/Login";
 import SignUpPage from "./pages/SignUp/SignUp";
 
@@ -11,10 +12,15 @@ page("/", async () => {
 page("/services", async () => {
   app.replaceChildren(ServicePage());
 });
+page("/pricing", async () => {
+  app.replaceChildren(PricingPage());
+});
+
 page("/login", async () => {
   app.replaceChildren(LoginPage());
 });
 page("/sign-up", async () => {
   app.replaceChildren(SignUpPage());
 });
+
 page();
